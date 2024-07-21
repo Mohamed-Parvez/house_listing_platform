defda7ef404f0238fccdc00d73796a1606606ab6
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-07-21 20:30:50 +0530

    removed next auth

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,44 +4,18 @@ interface HouseProps {
   houseName: string;
   housePrice: number;
 }
-import { AuthOptions } from "./api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-import UserAuthOptions from "@/components/UserAuthOptions";
 
 async function Home() {
   const fetchdata = await fetch("http://localhost:8080/", {
     cache: "no-store",
   });
-  const session = await getServerSession(AuthOptions);
-  console.log(`the session da ${session}`);
   const getdata: HouseProps[] = await fetchdata.json();
   return (
     <main className="flex flex-col items-start justify-start max-w-screen-xl w-full">
       <div className="max-w-screen-xl w-full p-4 flex items-center justify-between">
         <p className="text-[20px] font-medium">Welcome to house property</p>
-        <div className="flex items-center justify-center gap-6">
-          {session ? (
-            <UserAuthOptions />
-          ) : (
-            <Link
-              href={"/api/auth/signin"}
-              className="bg-black text-white rounded-[6px] hover:bg-white hover:text-black px-4 py-2 ring-1 ring-black"
-            >
-              Sign In
-            </Link>
-          )}
-          <Link href={"/housepost"}>
-            <button className="bg-black text-white rounded-[6px] hover:bg-white hover:text-black px-4 py-2 ring-1 ring-black">
-              upload houses
-            </button>
-          </Link>
-        </div>
       </div>
-      {session && (
-        <p className="p-4 text-[20px] font-medium text-black">
-          Hi, {session.user!.name}
-        </p>
-      )}
+
       <div className="flex flex-col items-start space-y-6 justify-start m-4">
         {getdata.map((items: any) => (
           <div
